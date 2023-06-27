0b2ec533c82bd684269c8e611785bcd23912656a
renting0109 <124872458+renting0109@users.noreply.github.com>
2023-06-27 15:57:50 +0800

    Add files via upload

---
+++ PHPT/ch03-01.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    // 差異
+    echo "Hello","World" ;//多帶入一參數
+    echo "<BR>";
+    print "Hello";
+    echo "<BR>";
+    //回傳
+    echo print "print 傳輸回傳";
+    echo "<BR>";
+
+    // 輸出
+    echo "Hello world!";
+    echo "<BR>";
+    print "Hello world!";
+
+    
+    
+    
+    
+    
+    ?>
+</body>
+</html>
---
+++ PHPT/ch06-01-postform.php
@@ -0,0 +1,7 @@
+
+    <?php
+    echo "Hello,";
+    echo $_POST["username"];
+ 
+    
+    ?>
